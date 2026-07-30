a3170d76f2d634728371ce9b00f23ecb83e92cdc
jayjaystdrl-blip <jayjay.stdr.l@gmail.com>
2026-07-30 16:26:12 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,22 +1,28 @@
-// swift-tools-version: 5.9
+// swift-tools-version:5.9
 
 import PackageDescription
 
 let package = Package(
     name: "UniversalPerformanceKit",
+
     platforms: [
         .iOS(.v15)
     ],
+
     products: [
         .library(
             name: "UniversalPerformanceKit",
-            targets: ["UniversalPerformanceKit"]
+            targets: [
+                "UniversalPerformanceKit"
+            ]
         )
     ],
+
     targets: [
         .target(
             name: "UniversalPerformanceKit",
-            path: "Sources"
+            path: "Sources/UniversalPerformanceKit",
+            publicHeadersPath: "include"
         )
     ]
 )
