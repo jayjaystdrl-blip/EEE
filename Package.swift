eadc7d25eda2d6a1643c036aef267813a9a797bb
jayjaystdrl-blip <jayjay.stdr.l@gmail.com>
2026-07-30 16:21:18 -0400

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "UniversalPerformanceKit",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "UniversalPerformanceKit",
+            targets: ["UniversalPerformanceKit"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "UniversalPerformanceKit",
+            path: "Sources"
+        )
+    ]
+)
