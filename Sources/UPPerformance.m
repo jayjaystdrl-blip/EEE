9dfebbde2a2805939530081908ba328d013c10d9
jayjaystdrl-blip <jayjay.stdr.l@gmail.com>
2026-07-30 16:45:36 -0400

    Update UPPerformance.m

--- Sources/UPPerformance.m
+++ Sources/UPPerformance.m
@@ -1,15 +1,17 @@
 #import "UPPerformance.h"
 #import "UPOverlayView.h"
+#import <UIKit/UIKit.h>
+
 
 @implementation UPPerformance
 
 + (instancetype)shared
 {
     static UPPerformance *instance;
-    static dispatch_once_t once;
+    static dispatch_once_t onceToken;
 
-    dispatch_once(&once, ^{
-        instance = [UPPerformance new];
+    dispatch_once(&onceToken, ^{
+        instance = [[UPPerformance alloc] init];
     });
 
     return instance;
@@ -20,43 +22,95 @@
 {
     dispatch_async(dispatch_get_main_queue(), ^{
 
-        UIWindow *window =
-        [[UIWindow alloc]
-         initWithFrame:UIScreen.mainScreen.bounds];
+        UIWindowScene *scene = nil;
+
+        for (UIScene *s in UIApplication.sharedApplication.connectedScenes)
+        {
+            if ([s isKindOfClass:UIWindowScene.class])
+            {
+                scene = (UIWindowScene *)s;
+                break;
+            }
+        }
+
+
+        UIWindow *window;
+
+
+        if (scene)
+        {
+            window =
+            [[UIWindow alloc]
+             initWithWindowScene:scene];
+        }
+        else
+        {
+            window =
+            [[UIWindow alloc]
+             initWithFrame:UIScreen.mainScreen.bounds];
+        }
+
+
+        UIViewController *controller =
+        [[UIViewController alloc] init];
+
+
+        window.rootViewController = controller;
+
+
+        UPOverlayView *overlay =
+        [[UPOverlayView alloc]
+         initWithFrame:CGRectMake(0,0,150,55)];
+
+
+        CGFloat width =
+        window.bounds.size.width;
+
+
+        CGFloat top =
+        window.safeAreaInsets.top;
+
+
+        overlay.center =
+        CGPointMake(width / 2,
+                    top + 35);
+
+
+        overlay.autoresizingMask =
+        UIViewAutoresizingFlexibleLeftMargin |
+        UIViewAutoresizingFlexibleRightMargin;
+
+
+        [controller.view addSubview:overlay];
+
 
         window.windowLevel =
         UIWindowLevelAlert + 1;
 
 
-        UPOverlayView *view =
-        [[UPOverlayView alloc]
-         initWithFrame:CGRectMake(0,0,150,55)];
-
-
-        view.center =
-        CGPointMake(
-            window.bounds.size.width / 2,
-            50
-        );
-
-
-        view.autoresizingMask =
-        UIViewAutoresizingFlexibleLeftMargin |
-        UIViewAutoresizingFlexibleRightMargin;
-
-
-        [window addSubview:view];
-
         window.hidden = NO;
+        window.alpha = 1.0;
+
+
+        self->_window = window;
 
     });
 }
 
+
 @end
+
 
 
 __attribute__((constructor))
 static void UPInitialize()
 {
-    [[UPPerformance shared] start];
+    NSLog(@"UniversalPerformance dylib loaded");
+
+
+    dispatch_async(dispatch_get_main_queue(), ^{
+
+        [[UPPerformance shared] start];
+
+    });
 }
