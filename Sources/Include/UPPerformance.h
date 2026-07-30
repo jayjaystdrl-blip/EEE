5d97a6b41f9b1ad776f8d2a7d6c8a91604cc56d5
jayjaystdrl-blip <jayjay.stdr.l@gmail.com>
2026-07-30 16:46:06 -0400

    Update UPPerformance.h

--- Sources/Include/UPPerformance.h
+++ Sources/Include/UPPerformance.h
@@ -1,8 +1,12 @@
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface UPPerformance : NSObject
 
+@property(nonatomic,strong) UIWindow *window;
+
 + (instancetype)shared;
+
 - (void)start;
 
 @end
